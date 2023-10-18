6de203b990f1e76277c24c2d404722fd55074289
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-10-18 19:36:33 -0300

    Configurando o React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+// pages
 
 function App() {
   return (
     <div className="App">
-      <h1>Miniblog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
