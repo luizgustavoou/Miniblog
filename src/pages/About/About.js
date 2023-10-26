b9f1784369926546b0e0b39e233f50c7750b0787
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-10-25 21:24:55 -0300

    Bloqueando páginas que precisam de autenticação

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,20 @@
 // CSS
 import styles from "./About.module.css";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h1>
+        Sobre o mini <span>Blog</span>
+      </h1>
+      <p>
+        Este projeto consiste em um blog feito com React no frontend e Firebase
+        no backend
+      </p>
+      <Link to="/post/create" className="btn">
+        Criar post
+      </Link>
     </div>
   );
 };
