75a46b9895885c682a6d550e2d908801a55d09bf
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-10-29 17:08:42 -0300

    Exibindo resultados da busca

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -38,7 +38,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
         if (search) {
           myQuery = await query(
             collectionRef,
-            where("tags", "array-contains", search),
+            where("tagsArray", "array-contains", search),
             orderBy("createdAt", "desc")
           );
         } else {
